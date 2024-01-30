431a6716ad2154d0f1c5bafa90db011d9e3c6fdd
elmerMg0 <mendozagutierrezelmer20@gmail.com>
2024-01-30 08:15:35 -0400

    module product

---
+++ src/models/models.ts
@@ -0,0 +1,33 @@
+export interface Product{
+    id?:number,
+    name: string,
+    code: string,
+    url_image?: string,
+    url_pdf?: string,
+    sale_price?: number,
+    cost_price?: number,
+    state: boolean | string,
+    description?: string,
+    brand_id?: number
+    category_id?: number
+}   
+
+export type PageInfo = {
+    count: number,
+    next:  string,
+    page: number,
+    previous:  string,
+    start: number,
+    totalPages: number
+} 
+
+export interface Category{
+    id?: number,
+    name: string,
+    description?: string
+}
+export interface Brand{
+    id?: number,
+    name: string,
+    description?: string
+}
---
+++ src/shared/btns/Button.tsx
@@ -0,0 +1,35 @@
+import React, { MouseEventHandler } from 'react'
+
+interface Props{
+    text?: string,
+    onClick?:  MouseEventHandler<HTMLButtonElement>;
+    variant: string,
+    children?: React.ReactNode
+    type?: "button" | "submit" | "reset"
+}
+function Button({text, onClick, variant, children, type = "button"}:Props) {
+  const classname = FactoryStyle(variant)
+  return (
+    <button type={type} onClick={onClick} className={`btn-gral ${classname}`}>
+      {children}
+      {text}
+    </button>
+  )
+}
+
+export default Button
+
+const FactoryStyle = (variant : string): string => {
+  switch (variant) {
+    case 'success':
+      return 'btn--success';
+    case 'error':
+        return 'btn--error';
+    case 'new':
+      return 'btn--new';
+    case 'main':
+        return 'btn--main';
+    default:
+       return '';
+  }
+}
