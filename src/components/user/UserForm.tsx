3d97e58c92c3126982e4a9d4594e8379a7eeb1b1
cesar <fcgvcesar@gmail.com>
2024-02-15 16:47:21 -0400

    fix/user/validation

--- src/components/user/UserForm.tsx
+++ src/components/user/UserForm.tsx
@@ -4,6 +4,19 @@ import FormField from "../../shared/FormField";
 import Button from "../../shared/btns/Button";
 import { User } from "../../models/models";
 
+declare module "yup" {
+  interface StringSchema {
+    isPasswordStrong(
+      options: {
+        minUppercase: number;
+        minLowercase: number;
+        minNumbers: number;
+        minSymbols: number;
+      },
+      errorMessage?: string
+    ): StringSchema;
+  }
+}
 
 interface CategoryFormProps {
   userToEdit: User;
@@ -11,28 +24,104 @@ interface CategoryFormProps {
   handleSubmit: (formData: User) => void;
   handleCancel: () => void;
 }
-const CategoryFormSchema = Yup.object().shape({
-  name: Yup.string().required("* El nombre es obligatorio"),
-  username: Yup.string().required('El nombre de usuario es obligatorio'),
-  phone: Yup.string().matches(/^\d+$/, 'Ingrese un número de teléfono válido'),
-  password: Yup.string().min(6, 'La contraseña debe tener al menos 6 caracteres')
-  //rol: Yup.string().required('Seleccione un rol').oneOf(['Seller', 'Admin'], 'Rol no válido'),
-  //state: Yup.string().required('Seleccione un estado').oneOf(['1', '0'], 'Estado no válido'),
-  
-});
+
+Yup.addMethod(
+  Yup.string,
+  "isPasswordStrong",
+  function (options, errorMessage = "La contraseña debe ser más fuerte") {
+    return this.test("test-is-password-strong", errorMessage, function (value) {
+      const { path, createError } = this;
+
+      let isStrong = true;
+
+      if (typeof value === "undefined") {
+        return true;
+      }
+
+      if (options?.minLowercase) {
+        if (!/[a-z]/.test(String(value))) {
+          isStrong = false;
+        }
+      }
+      if (options?.minUppercase) {
+        if (!/[A-Z]/.test(String(value))) {
+          isStrong = false;
+        }
+      }
+      if (options?.minNumbers) {
+        if (!/\d/.test(String(value))) {
+          isStrong = false;
+        }
+      }
+      if (options?.minSymbols) {
+        if (!/\W/.test(String(value))) {
+          isStrong = false;
+        }
+      }
+
+      return (
+        isStrong ||
+        createError({
+          path,
+          message: errorMessage,
+        })
+      );
+    });
+  }
+);
+
 export default function UserForm({
   userToEdit,
   formData,
   handleSubmit,
   handleCancel,
 }: CategoryFormProps) {
+  const CategoryFormSchema = Yup.object().shape({
+    name: Yup.string()
+      .required("* El nombre es obligatorio")
+      .min(2, "El nombre debe tener al menos 2 caracteres")
+      .max(80, "El nombre no debe tener más de 80 caracteres"),
+    username: Yup.string()
+      .required("El nombre de usuario es obligatorio")
+      .min(3, "El nombre de usuario debe tener al menos 3 caracteres")
+      .max(50, "El nombre de usuario no debe tener más de 50 caracteres"),
+      phone: Yup.string()
+      .matches(/^([67])?\d{7,8}$/, "Ingrese un número de teléfono válido en Bolivia")
+      .required("El número de teléfono es obligatorio"),
+
+    password:
+      userToEdit.id == 0
+        ? Yup.string()
+            .required("Campo requerido.")
+            .min(8, "La contraseña es muy corta")
+            .isPasswordStrong(
+              {
+                minUppercase: 1,
+                minLowercase: 1,
+                minNumbers: 1,
+                minSymbols: 1,
+              },
+              "La contraseña debe contener al menos una mayúscula, una minúscula, un número y un carácter especial."
+            )
+        : Yup.string().min(8, "La contraseña es muy corta").isPasswordStrong(
+            {
+              minUppercase: 1,
+              minLowercase: 1,
+              minNumbers: 1,
+              minSymbols: 1,
+            },
+            "La contraseña debe contener al menos una mayúscula, una minúscula, un número y un carácter especial."
+          ),
+    role: Yup.string()
+      .required("Seleccione un rol")
+      .oneOf(["Admin", "Seller"], "Rol no válido"),
+  });
   return (
     <Formik
       initialValues={formData}
       validationSchema={CategoryFormSchema}
       onSubmit={(values) => {
         handleSubmit(values);
-
       }}
     >
       <Form>
