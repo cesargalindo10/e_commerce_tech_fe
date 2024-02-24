eaa993ee01520aaee42c4c27bce61506078b1ea6
cesar <fcgvcesar@gmail.com>
2024-02-24 11:02:54 -0400

    delete clg

--- src/infrastructure/api/api.service.ts
+++ src/infrastructure/api/api.service.ts
@@ -54,8 +54,6 @@ export const APISERVICE = {
         }
 
         const data = await response.json(); 
-        console.log(data);
-
         return data;
     } catch (error) {
         console.error('Error en la solicitud:', error);
